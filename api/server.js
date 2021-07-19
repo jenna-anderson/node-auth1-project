179b78fdcc526e45c5c1a1b8cf66c1a3678c8319
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-19 14:55:04 -0600

    uses express-session to persist a users session on the server

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const usersRouter = require('./users/users-router')
+const session = require('express-session');
+const store = require('connect-session-knex')(session)
 
+const usersRouter = require('./users/users-router');
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -18,6 +20,25 @@ const usersRouter = require('./users/users-router')
 
 const server = express();
 
+server.use(session({
+  name: 'chocolatechip',
+  secret: 'this should come from an env var',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: false,
+  },
+  rolling: true,
+  resave: false,
+  saveUninitialized: false,
+  store: new store({
+    knex: require('../data/db-config'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  })
+}));
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -11,22 +11,29 @@ async function find() {
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
-function findBy(filter) {
-
+async function findBy(filter) {
+  const filteredUsers = await db('users').where('username', 'like', `%${filter}%`)
+  return filteredUsers
 }
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
-function findById(user_id) {
-
+async function findById(user_id) {
+  const [user] = await db
+    .select('user_id', 'username')
+    .from('users')
+    .where('user_id', user_id)
+  return user
 }
 
 /**
   resolves to the newly inserted user { user_id, username }
  */
-function add(user) {
-
+async function add(user) {
+  const [id] = await db.insert(user)
+  const newUser = await db.findById(id)
+  return newUser
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
