df8d1c7a72ad819786044bdcc084691167e61351
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-19 17:19:58 -0600

    sends appropriate status code and error message if use tries to register with a password less than 3 chars

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -34,14 +34,6 @@ async function checkUsernameFree(req, res, next) {
   }
 }
 
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
-*/
 async function checkUsernameExists(req, res, next) {
   const exists = await Users.findBy(req.body.username)
   if (exists.length !== 0) {
@@ -54,18 +46,9 @@ async function checkUsernameExists(req, res, next) {
   }
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
-*/
 function checkPasswordLength(req, res, next) {
   const { password } = req.body
-  console.log(password.length < 4)
-  if(password.length < 4) {
+  if(!password || password.length < 4) {
     next({
       status: 422,
       message: "Password must be longer than 3 chars"
@@ -75,7 +58,6 @@ function checkPasswordLength(req, res, next) {
   }
 }
 
-// Don't forget to add these to the `exports` object so they can be required in other modules
 module.exports = {
   restricted,
   checkUsernameFree,
